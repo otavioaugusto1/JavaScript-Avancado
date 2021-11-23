05456f81b650c86d2496cd6fe79f6a0698c936c4
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-11-23 13:56:21 -0300

    let

---
+++ JS iniciante/let.js
@@ -0,0 +1,9 @@
+/**
+ * var tem escopo somente de função
+ * ou seja, se for instanciada dentro de um if
+ * será visível no resto inteiro da função
+ * 
+ * Já o let é para qualquer bloco ({},if,for,function e etc...)
+ * O let fica só para o seu escopo
+ * 
+ */
