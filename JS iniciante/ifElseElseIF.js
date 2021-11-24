9a44b8e5b8603152dceff66228a33eacfbecab5d
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-11-24 15:21:44 -0300

    If else else if

---
+++ JS iniciante/ifElseElseIF.js
@@ -0,0 +1,5 @@
+const hora = 50;
+
+if(hora> 10){
+  console.log(`${hora} é maior que 10`)
+}
---
+++ JS iniciante/ternario.js
@@ -0,0 +1,11 @@
+const pontuacaoUsuario = 999;
+
+if(pontuacaoUsuario >= 1000){
+  console.log("Usuário é VIP")
+}else{
+  console.log("Usuário não é VIP")
+}
+
+const TipoUsuaio = pontuacaoUsuario >= 1000 ? "VIP" : "Não VIP"
+
+console.log(TipoUsuaio)
