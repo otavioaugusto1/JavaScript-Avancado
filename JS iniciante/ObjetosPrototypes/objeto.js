5e5ce6a0066108b95b74e5d732b8403b3fd0287e
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2022-03-02 09:54:39 -0300

    objeto

--- JS iniciante/ObjetosPrototypes/objeto.js
+++ JS iniciante/ObjetosPrototypes/objeto.js
@@ -1,1 +1,21 @@
 // literal
+const pessoa = {
+  nome: "Otávio",
+  sobrenome: "Augusto",
+  idade: 22,
+  endereco: {
+    rua: "Monsenhor Aluísio",
+    bairro: "Juazeiro"
+  },
+  falar(){
+    console.log("Olá")
+  }
+}
+console.log(pessoa.endereco.bairro)
+
+// deletando uma chave
+
+delete pessoa.sobrenome
+console.log(pessoa)
+
+pessoa.falar()
