fbbb4878034d33eb8b937bf5ab4ff8403c2e421f
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2022-03-02 09:25:10 -0300

    adding desestruturacao and object

--- JS iniciante/desestruturacao.js
+++ JS iniciante/desestruturacao.js
@@ -1,9 +1,10 @@
 let a = 'A' // B
 let b = 'B' // C
 let c = 'C' // A
+let resto = 'UI'
 console.log(a, b, c)
 
 const numeros = [1,2,3,4,5];
 
-[a, b, c] = numeros;
-console.log(a, b, c)
+[a, b, c, ...rest] = numeros;
+console.log(a, b, c, resto)
